9be9690de3bb284f4b615772773556bba91ec4bf
Dario Rivas <palkiadialgalugia@gmail.com>
2025-05-16 23:01:52 +0200

    shell1.1

--- OS/kernel.asm
+++ OS/kernel.asm
@@ -3,19 +3,23 @@ org 0x1000
 bits 16
 
 start:
-; %include "libs/intro.inc" 
+
 call limpiar_pantalla
 mov si, Mensaje_shell
 call imprimir_cadena
 jmp shell
 
 shell:
-
+        call leer_comando
         call leer_caracter
         mov al, [char]
 
         cmp al, 0x08
         je borrar_caracter
+
+        cmp al, 0x0D
+        je enter_shell
+
 
         call imprimir_caracter
         jmp shell
