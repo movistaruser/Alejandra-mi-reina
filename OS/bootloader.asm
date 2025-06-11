05f3171c2267aebd5dc6914174e3dec0992f989d
Dario Rivas <palkiadialgalugia@gmail.com>
2025-06-11 21:27:55 +0200

    DariOS 1.0

--- OS/bootloader.asm
+++ OS/bootloader.asm
@@ -12,20 +12,18 @@ main:
     push cx
     push dx
 
-
-    mov ah, 0x06        ; función de scroll up
-    mov al, 0           ; 0 = limpiar toda la pantalla
-    mov bh, 0x07     ; atributo: fondo negro, texto gris claro (07h)
-    mov cx, 0x0000      ; esquina superior izquierda (fila 0, columna 0)
-    mov dx, 0x184F      ; esquina inferior derecha (fila 24, columna 79)
+    mov ah, 0x06       
+    mov al, 0           
+    mov bh, 0x07    
+    mov cx, 0x0000      
+    mov dx, 0x184F      
     int 0x10
 
-
-    mov ah, 0x02        ; función de mover cursor
-    mov bh, 0x00        ; página 0
-    mov dh, 0x09       ; fila 12 (centro vertical)
-    mov dl, 0x25        ; columna 40 (centro horizontal)
-    int 0x10            ; llamada a la interrupción del BIOS para mover el 
+    mov ah, 0x02       
+    mov bh, 0x00    
+    mov dh, 0x09      
+    mov dl, 0x25       
+    int 0x10           
 
     pop dx
     pop cx
@@ -40,11 +38,11 @@ main:
     push cx
     push dx
 
-    mov ah, 0x02        ; función de mover cursor
-    mov bh, 0x00        ; página 0
-    mov dh, 0x0b       ; fila 12 (centro vertical)
-    mov dl, 0x19        ; columna 40 (centro horizontal)
-    int 0x10            ; llamada a la interrupción del BIOS para mover el 
+    mov ah, 0x02        
+    mov bh, 0x00        
+    mov dh, 0x0b      
+    mov dl, 0x19        
+    int 0x10            
 
     pop dx
     pop cx
@@ -63,13 +61,10 @@ main:
     mov bx, 0x1000
     int 0x13
 
-
-jmp 0x1000
-
+    jmp 0x1000
 
     %include "libs/variables.inc"
     %include "libs/funciones_globales.inc"
-
 
 times 510-($-$$) db 0 
 dw 0xAA55 
